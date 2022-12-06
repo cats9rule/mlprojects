a04f7d0a572ef20d99bda15afd3cad2f46736d56
niksa0522 <nikola.ilic.bs@elfak.rs>
2022-12-06 21:49:54 +0100

    Modified notebook
    Added Standard Scaler
    Changed text values for percentages

--- p1/scrapped.py
+++ p1/scrapped.py
@@ -40,3 +40,41 @@ df_cfm = pd.DataFrame(result, index = classes, columns = classes)
 plt.figure(figsize = (10,7))
 cfm_plot = sns.heatmap(df_cfm, annot=True)
 cfm_plot.figure.savefig("evaluation\cfm.png")
+
+
+
+
+# deo koda koji je predstavljao spajanje vise datasetova u dataConcatinated
+dataTest = pd.read_csv("occupancy_data/datatest.txt", sep=',')
+dataTest['date'] = [pd.to_datetime(dataTest, format='%Y-%m-%d %H:%M:%S') for dataTest in dataTest['date']]
+dataTest['date'] = pd.DatetimeIndex(dataTest.date).asi8
+
+dataTraining = pd.read_csv("occupancy_data/datatraining.txt", sep=',')
+dataTraining['date'] = [pd.to_datetime(dataTraining, format='%Y-%m-%d %H:%M:%S') for dataTraining in dataTraining['date']]
+dataTraining['date'] = pd.DatetimeIndex(dataTraining.date).asi8
+
+dataConcatinated = pd.concat([data,dataTest,dataTraining],axis=0)
+print(f"Broj uzoraka u kompletnom skupu podataka: {len(dataConcatinated)}")
+
+cols = [col for col in dataConcatinated.columns if col not in ['Occupancy']]
+dataConcatinated_features = dataConcatinated[cols]
+targetConcatinated = dataConcatinated['Occupancy']
+
+data_train,data_test,target_train,target_test = train_test_split(dataConcatinated_features,targetConcatinated,test_size=0.2,random_state=10)
+
+print(f"Broj uzoraka u trening skupu podataka: {len(data_train)}")
+print(f"Broj uzoraka u test skupu podataka: {len(data_test)}")
+
+
+scaler = preprocessing.MinMaxScaler()
+
+data_train = scaler.fit_transform(data_train)
+data_test = scaler.transform(data_test)
+
+pred = svc_model.fit(data_train, target_train).predict(data_test)
+
+print("LinearSVC accuracy : ",accuracy_score(target_test, pred, normalize = True))
+report = classification_report(target_test,pred)
+with io.open('evaluation/classification_report_scaler_concatinated.txt','w',encoding='utf-8') as f: f.write(report)
+result = confusion_matrix(target_test, pred)
+print(result)
